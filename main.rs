d9638d22cc34013fcad6613f5190f390716b2fa6
Julien DURIS <monkeydioude@gmail.com>
2021-02-05 23:08:28 +0100

    F

--- main.rs
+++ main.rs
@@ -1,13 +1,14 @@
-use std::{fmt::{Display, Formatter, Result}, ops::{Add, Mul}};
+use std::{fmt::{Display, Formatter, Result}, io::stdin, ops::{Add, Mul, Sub}};
+
 // Vector2D is a simple 2 dimensional struct
 // for force computation.
 struct Vector2D {
-    x: i64,
-    y: i64,
+    x: f64,
+    y: f64,
 }
 
 impl Vector2D {
-    pub fn new(x: i64, y: i64) -> Vector2D {
+    pub fn new(x: f64, y: f64) -> Vector2D {
         Vector2D {x, y}
     }
 }
@@ -32,8 +33,16 @@ impl Add<Vector2D> for Vector2D {
     }
 }
 
-// Add<Vector2D> allows "+" operation over a Vector2D and an i64 
-impl Add<Vector2D> for i64 {
+impl Sub<Vector2D> for Vector2D {
+    type Output = Vector2D;
+
+    fn sub(self, other: Vector2D) -> Vector2D {
+        Vector2D::new(self.x - other.x, self.y - other.y)
+    }
+}
+
+// Add<Vector2D> allows "+" operation over a Vector2D and an f64 
+impl Add<Vector2D> for f64 {
     type Output = Vector2D;
 
     fn add (self, other: Vector2D) -> Vector2D {
@@ -41,11 +50,11 @@ impl Add<Vector2D> for i64 {
     }
 }
 
-// Add<Vector2D> allows "+" operation over an i64 and a Vector2D
-impl Add<i64> for Vector2D {
+// Add<f64> allows "+" operation over an f64 and a Vector2D
+impl Add<f64> for Vector2D {
     type Output = Vector2D;
 
-    fn add (self, v: i64) -> Vector2D {
+    fn add (self, v: f64) -> Vector2D {
         Vector2D::new(self.x + v, self.y + v)
     }
 }
@@ -59,8 +68,8 @@ impl Mul<Vector2D> for Vector2D {
     }
 }
 
-// Mul<Vector2D> allows "*" operation over a Vector2D and an i64 
-impl Mul<Vector2D> for i64 {
+// Mul<Vector2D> allows "*" operation over a Vector2D and an f64 
+impl Mul<Vector2D> for f64 {
     type Output = Vector2D;
 
     fn mul (self, other: Vector2D) -> Vector2D {
@@ -68,22 +77,56 @@ impl Mul<Vector2D> for i64 {
     }
 }
 
-// Mul<Vector2D> allows "*" operation over an i64 and a Vector2D
-impl Mul<i64> for Vector2D {
+// Mul<f64> allows "*" operation over an i64 and a Vector2D
+impl Mul<f64> for Vector2D {
     type Output = Vector2D;
 
-    fn mul (self, v: i64) -> Vector2D {
+    fn mul (self, v: f64) -> Vector2D {
         Vector2D::new(self.x * v, self.y * v)
     }
 }
 
 
-fn main() {
-    let a: i64 = 3;
-    let b: i64 = 2;
-    let c = Vector2D::new(2, 3);
+fn get_stdin_line() -> Option<String> {
+    let mut input = String::new();
 
-    let t = (a + b) * c * 2;
+    match stdin().read_line(&mut input) {
+        Ok(_) => {
+            Some(input.trim().to_owned())
+        },
+        _ =>  {
+            println!("WESH !!");
+            None
+        }
+    }
+
+}
+
+fn to_f64(str: String) -> Option<f64> {
+    use std::str::FromStr;
+
+    match f64::from_str(&str) {
+        Ok(r) => Some(r),
+        _ => {
+            println!("WESH !!");
+            None
+        }
+    }
+}
+
+fn main() {
+    println!("V1, x:");
+    let x1 = to_f64(get_stdin_line().unwrap()).unwrap();
+    println!("V1, y:");
+    let y1 = to_f64(get_stdin_line().unwrap()).unwrap();
+    println!("V2, x:");
+    let x2 = to_f64(get_stdin_line().unwrap()).unwrap();
+    println!("V2, y:");
+    let y2 = to_f64(get_stdin_line().unwrap()).unwrap();
+
+    let x = Vector2D::new(x1, y1);
+    let y = Vector2D::new(x2, y2);
+    let t = x - y;
 
     println!("{}", t);
 }
