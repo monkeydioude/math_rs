ec0fb0d9597229c4e033d08550e391cf8c958073
mkd <monkeydioude@gmail.com>
2021-02-09 23:56:04 +0100

    added tests

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,8 @@ fn to_f64(str: String) -> f64 {
 }
 
 fn main() {
-    let m1:f64 = 5.972e+24;
-    let m2:f64 = 1.989e+30;
+    let m1:f64 = 1.989e+30;
+    let m2:f64 = 5.972e+24;
     // let v1 = Vector2D::new(
     //     to_f64(get_stdin_line("V1, x:")), 
     //     to_f64(get_stdin_line("V1, y:")),
@@ -42,18 +42,19 @@ fn main() {
     //     to_f64(get_stdin_line("V2, y:")),
     // );
     let v1 = Vector2D::new(
-        750000000000.0, 
-        750000000000.0,
+        0.,
+        0.,
     );
     let v2 = Vector2D::new(
-        0.0, 
-        0.0,
+        -149.96e9,
+        0.,
     );
     let r21_v = v2 - v1;
     let dist = r21_v.norm_abs();
+    println!("{}", &dist);
     let g = 6.674e-11;
     
     let f = (g*m1*m2) / (dist*dist);
     let f_v = -f * (r21_v/dist);
-    println!("dist\t= {}\nF\t= {}\nF21->\t= {}\nF21->2\t= {}\nacc(m1)\t= {}\nacc(m2)\t= {}", dist, f, f_v, (-g*m1*m2*r21_v)/dist/dist/dist, (-g*m2*r21_v)/dist/dist/dist, (-g*m1*r21_v)/dist/dist/dist);
+    println!("dist\t= {}\nF\t= {}\nF21->\t= {}\nF21->2\t= {}\nacc(m1)\t= {}\nacc(m2)\t= {}", dist, f, f_v, (-g*m1*m2*r21_v)/dist/dist/dist, f_v/m1, f_v/m2);
 }
--- src/vector2d.rs
+++ src/vector2d.rs
@@ -2,6 +2,7 @@ use std::{fmt::{Display, Formatter, Result}, ops::{Add, Div, Mul, Sub}};
 
 // Vector2D is a simple 2 dimensional struct
 // for force computation.
+#[derive(Debug)]
 pub struct Vector2D {
     x: f64,
     y: f64,
@@ -47,14 +48,6 @@ impl Add<Vector2D> for Vector2D {
 }
 
 impl Sub<Vector2D> for Vector2D {
-    type Output = Vector2D;
-
-    fn sub(self, other: Vector2D) -> Vector2D {
-        Vector2D::new(self.x - other.x, self.y - other.y)
-    }
-}
-
-impl Sub<Vector2D> for &Vector2D {
     type Output = Vector2D;
 
     fn sub(self, other: Vector2D) -> Vector2D {
@@ -119,14 +112,72 @@ impl Div<f64> for Vector2D {
     }
 }
 
-// Div<f64> allows "/" (divide) operation over a f64 and a borrowed Vector2D
-impl Div<f64> for &Vector2D {
-    type Output = Vector2D;
+#[cfg(test)]
+mod tests {
+    use super::Vector2D;
 
-    fn div(self, v: f64) -> Vector2D {
-        if v == 0f64 {
-            return Vector2D::new(0f64, 0f64)
-        }
-        Vector2D::new(self.x / v, self.y / v)
+    #[test]
+    fn i_can_add_2_vector2d() {
+        let goal = Vector2D::new(3., 3.);
+        let trial = Vector2D::new(1., 1.) + Vector2D::new(2., 2.);
+        assert_eq!(trial.x, goal.x);
+        assert_eq!(trial.y, goal.y);
+    }
+
+    #[test]
+    fn i_can_add_f64_w_vector2d() {
+        let goal = Vector2D::new(69., 420.);
+        let trial = Vector2D::new(36., 387.) + 33.;
+        assert_eq!(trial.x, goal.x);
+        assert_eq!(trial.y, goal.y);
+    }
+
+    #[test]
+    fn i_can_multiply_2_vector2d() {
+        let goal = Vector2D::new(1_870.869_000_000_000_1, 2.742_9);
+        let trial = Vector2D::new(4.45, 1.23) * Vector2D::new(420.42, 2.23);
+        assert_eq!(trial.x, goal.x);
+        assert_eq!(trial.y, goal.y);
+    }
+
+    #[test]
+    fn i_can_multiply_f64_w_vector2d() {
+        let goal = Vector2D::new(96.024, 420.69);
+        let trial = 33. * Vector2D::new(2.9098181818181818181818181818182, 12.748181818181818181818181818182);
+        assert_eq!(trial.x, goal.x);
+        assert_eq!(trial.y, goal.y);
+    }
+
+    #[test]
+    fn i_can_substract_2_vector2d() {
+        let goal = Vector2D::new(2.5, -25.);
+        let trial = Vector2D::new(12., 50.) - Vector2D::new(9.5, 75.);
+        assert_eq!(trial.x, goal.x);
+        assert_eq!(trial.y, goal.y);
+    }
+
+    #[test]
+    fn i_can_divide_vector2d_w_f64() {
+        let goal = Vector2D::new(45., 0.25);
+        let trial = Vector2D::new(180., 1.) / 4.;
+        assert_eq!(trial.x, goal.x);
+        assert_eq!(trial.y, goal.y);
+    }
+
+    #[test]
+    fn i_can_compute_norm_abs() {
+        let goal = 5.;
+        let trial = Vector2D::new(4., -3.);
+        assert_eq!(trial.norm_abs(), goal);
+    }
+
+    #[test]
+    fn i_can_be_the_new_newton_ton() {
+        let goal = Vector2D::new(35252499978477410000000.0,-0.0);
+        let r21_v = Vector2D::new(-149.96e9, 0.) - Vector2D::new(0., 0.);
+        let dist = r21_v.norm_abs();
+        let trial = (-6.674e-11*1.989e+30*5.972e+24*r21_v)/dist/dist/dist;
+        assert_eq!(trial.x, goal.x);
+        assert_eq!(trial.y, goal.y);
     }
 }
