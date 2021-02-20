de525b37a6eab040eabbcae5f9a2f53ec2f9a97d
mkd <monkeydioude@gmail.com>
2021-02-20 10:57:22 +0100

    sdl ?

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod vector2d;
-// mod big_float;
 
 use std::{io::stdin, str::FromStr};
 use vector2d::Vector2D;
+
+extern crate sdl2;
 
 #[allow(dead_code)]
 fn get_stdin_line(msg: &str) -> String {
@@ -30,17 +31,10 @@ fn to_f64(str: String) -> f64 {
     }
 }
 
-fn main() {
+#[allow(dead_code)]
+fn run_newton_run() {
     let m1:f64 = 1.989e+30;
     let m2:f64 = 5.972e+24;
-    // let v1 = Vector2D::new(
-    //     to_f64(get_stdin_line("V1, x:")), 
-    //     to_f64(get_stdin_line("V1, y:")),
-    // );
-    // let v2 = Vector2D::new(
-    //     to_f64(get_stdin_line("V2, x:")), 
-    //     to_f64(get_stdin_line("V2, y:")),
-    // );
     let v1 = Vector2D::new(
         0.,
         0.,
@@ -57,4 +51,11 @@ fn main() {
     let f = (g*m1*m2) / (dist*dist);
     let f_v = -f * (r21_v/dist);
     println!("dist\t= {}\nF\t= {}\nF21->\t= {}\nF21->2\t= {}\nacc(m1)\t= {}\nacc(m2)\t= {}", dist, f, f_v, (-g*m1*m2*r21_v)/dist/dist/dist, f_v/m1, f_v/m2);
+}
+
+fn main() {
+    let _sdl = match sdl2::init() {
+        Ok(s) => s,
+        Err(err) => panic!("Problem initializing sdl: {:?}", err),
+    };
 }
